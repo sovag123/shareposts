73e6fe3ed39617253adf61f1e3e51e6c2a900c37
sovag123 <vasilijevanja@gmail.com>
2021-04-27 00:33:10 +0200

    Initial commit

---
+++ app/config/config.php
@@ -0,0 +1,12 @@
+<?php
+
+define ("APPROOT" , dirname(dirname(__FILE__)));
+define ("URLROOT","http://37.0.71.52/s4f.test/Bozaric_Vasilije_S3A/shareposts/");
+define ("SITENAME", "SharePosts");
+define('APPVERSION', '1.0.0');
+
+define("DB_HOST" , "localhost");
+define("DB_USER" , "ucenik");
+define("DB_PASS" , "1234");
+define("DB_NAME" , "ucenik");
+?>
